550292241ad6d5cac8409f964d8f8ac3f9707deb
Oluwaseun Jubril <bellomsean@gmail.com>
2018-08-13 18:11:00 +0100

    Add files via upload

---
+++ like_button.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'Awesome!! You liked this, click the picture to make use of the App ' ;
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    ReactDOM.render(
+      e(LikeButton), domContainer);
+  });
