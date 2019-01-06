800bae99160c2b46deb5133c2459e19ad3459357
Oluwaseun Jubril <bellomsean@gmail.com>
2019-01-06 16:04:40 +0100

    Update slide.js

--- slide.js
+++ slide.js
@@ -22,8 +22,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  setTimeout("showSlides(slides, dots)", time);
+  setTimeout("showSlides()", time);
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
-window.onload = showSlides;
+window.onload = showSlides();
