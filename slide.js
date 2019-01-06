dc543e9932f93ab703c0bf957ba30894f2d54ec6
Oluwaseun Jubril <bellomsean@gmail.com>
2019-01-06 16:16:48 +0100

    Update slide.js

--- slide.js
+++ slide.js
@@ -24,6 +24,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout("showSlides()", time);
 }
-window.onload = showSlides;
